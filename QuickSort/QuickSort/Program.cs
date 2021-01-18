17955d33c68cf175c0aa78a11898bcfb4a162ebb
Oxana Ushakova <ushakova.oxana@gmail.com>
2021-01-18 19:02:50 +0100

    adding QuickSort

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 10, 7, 8, 9, 1, 5 };
+            int n = arr.Length;
+
+            QuickSort.quickSort(arr, 0, n - 1);
+            Console.WriteLine("sorted array ");
+            QuickSort.printArray(arr, n);
+        }
+    }
+}
